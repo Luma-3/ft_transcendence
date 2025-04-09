8ae16bc3182cdcebbd00819c3487cf65119a9301
Luma-3 <jb.brs@icloud.com>
2025-04-09 23:49:18 +0200

    add knex config deleted by merge

---
+++ backend/user-API/config/knex.config.mjs
@@ -0,0 +1,25 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config()
+
+const config = {
+	development: {
+		client: 'sqlite3',
+		connection: {
+			filename: './data/User.sqlite',
+
+		},
+		migrations: {
+			directory: path.join(process.cwd(), 'migrations'),
+			tableName: 'knex_schema_history',
+			loadExtensions: ['.mjs']
+		},
+		seeds: {
+			directory: path.join(process.cwd(), 'seeds', 'sandbox')
+		},
+		useNullAsDefault: true
+	},
+};
+
+export default config;
