1e058b4f19793fcfca02116433d20e97b8c51717
Luma-3 <jb.brs@icloud.com>
2025-04-09 17:40:35 +0200

    merge fix ok

---
+++ frontend/website/src/events/userSession/userLogIn.ts
@@ -0,0 +1,36 @@
+import { renderPage } from '../../components/renderPage'
+
+export async function loginUser() {
+    const username = document.forms["loginForm"]["loginUsername"].value;
+    const password = document.forms["loginForm"]["loginPassword"].value;
+
+    //requete verif username existe ou pas
+    if (username === "")        { renderPage('login') } // TODO : redirection page d'erreur et/ou page de redirection (tenter de modifier le code style "you have been hacked")
+    if (password === "")        { renderPage('login') }
+
+    //requete vers backend
+    let data = null;
+    try {
+        const response = await fetch('http://localhost:3000/api/user', {
+            method: "POST",
+            body: JSON.stringify({
+                username: username,
+                password: password
+            }),
+            headers: {
+                "Content-type": "application/json"
+              }
+        })
+        data = await response.json();
+    } catch (error) {
+        console.error('Fetch error:', error);
+    }
+    // if not connected : refresh log page
+    // if connected : go to home
+    if (data === null) {
+        renderPage('login')
+    }
+    console.log('fin du post', data);
+    renderPage('home');
+    window.alert("username or password incorrect")
+}
---
+++ frontend/website/src/main.ts
@@ -0,0 +1,37 @@
+import { renderPage } from './components/renderPage'
+import { addAllEventListenOnPage } from './events/Handler'
+import { initGoogleClient } from './components/Google'
+import { fadeIn } from './components/utils/fade'
+
+const main_container = document.querySelector<HTMLDivElement>('#app')!
+
+export function addToHistory(page: string, updateHistory: boolean = true) {
+    localStorage.setItem('current_page', page)
+    if (updateHistory && page !== history.state?.page) {
+        history.pushState({ page }, '', `/${page}`)
+    }
+}
+
+addAllEventListenOnPage(main_container);
+
+// * Au chargement initial ou refresh de la page
+// * On initialise le client Google
+// * On affiche la page courante ou la page d'accueil par défaut avec un leger delai
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const page =  window.location.pathname.substring(1) || 'home'
+    
+    initGoogleClient();
+    renderPage(page, false)
+    
+    setTimeout(() => {
+        fadeIn(main_container)
+    }, 1900);
+});
+
+// * Au changement de page lors de l'utilisation du bouton back/forward
+window.addEventListener('popstate', (event) => { 
+    console.log('popstate')
+    const page = event.state?.page || 'home'
+    renderPage(page, false)
+});
