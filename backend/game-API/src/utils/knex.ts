fd2522b6bd1a05c8f342dc76cb0b4ad4b4c3e240
Antoine Astruc <ajourdanastruc@gmail.com>
2025-07-15 12:45:50 +0200

    Db tournament in progress

---
+++ backend/game-API/src/utils/knex.ts
@@ -0,0 +1,32 @@
+import knex from 'knex';
+import type { Knex } from 'knex';
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config();
+const __dirname = import.meta.dirname;
+
+const config: { [key: string]: Knex.Config } = {
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/User.db',
+    },
+    migrations: {
+      directory: path.resolve(__dirname, '../../migrations'),
+      tableName: 'knex_schema_history',
+    },
+    useNullAsDefault: true
+  },
+};
+
+
+const env = process.env.NODE_ENV || 'development';
+export const knexInstance: Knex = knex(config[env]);
+
+const destroyKnex = async () => {
+  if (knexInstance) {
+    await knexInstance.destroy();
+  }
+}
+
+export { Knex, destroyKnex, config };
